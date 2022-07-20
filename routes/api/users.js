33fed2086177ad43251a715193a7c14fbcaf2542
akhil02hero <qingjianmaochase@gmail.com>
2022-07-19 23:36:49 -0400

    Initial commit of all files to the repository

---
+++ routes/api/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+//@route POST api/users
+//@desc Register Users
+//@access Public
+
+router.post('/',[
+    check('name',  'Name is Required').not().isEmpty(),
+    check('email', 'Give a Valid Email').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+], 
+(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()})
+    }
+    res.send('user page');
+});
+
+module.exports = router
